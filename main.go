2245b0c11ec68fe8b4729f3809e8fa6d9d884a85
Geleta116 <geleta.dabakumsa@gmail.com>
2024-08-10 11:59:52 +0300

    bug fix

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
     "os"
     "os/exec"
     "syscall"
-    "golang.org/x/sys/unix"
+	"golang.org/x/sys/unix"
+)
+
+const (
+    CLONE_NEWUTS  = 0x04000000
+    CLONE_NEWPID  = 0x20000000
+    CLONE_NEWNS   = 0x00020000
 )
 
 func main() {
@@ -15,15 +21,30 @@ func main() {
     case "child":
         child()
     default:
-        panic("wat should I do")
-    }
-}
-
+        panic("not working")}}
+    
 func parent() {
-    cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-    cmd.SysProcAttr = &syscall.SysProcAttr{
-        Cloneflags: unix.CLONE_NEWUTS | unix.CLONE_NEWPID | unix.CLONE_NEWNS,
-    }
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...))
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: unix.CLONE_NEWUTS | unix.CLONE_NEWPID | unix.CLONE_NEWNS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("ERROR", err)
+		os.Exit(1)
+	}}
+		
+
+func child() {
+    must(unix.Mount("rootfs", "rootfs", "", unix.MS_BIND, ""))
+    must(os.MkdirAll("rootfs/oldrootfs", 0700))
+    must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
+    must(os.Chdir("/"))
+
+    cmd := exec.Command(os.Args[2], os.Args[3:]...)
     cmd.Stdin = os.Stdin
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
@@ -34,26 +55,8 @@ func parent() {
     }
 }
 
-
-func child() {
-	must(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
-	must(os.MkdirAll("rootfs/oldrootfs", 0700))
-	must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
-	must(os.Chdir("/"))
-
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
-		os.Exit(1)
-	}
-}
-
 func must(err error) {
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 }
